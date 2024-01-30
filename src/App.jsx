768dbbac21ef2d113fa69f7d3d69294b29ae3d9f
unknown <carlseighartaliode@gmail.com>
2024-01-30 21:58:18 +0800

    added navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import Navbar from './components/navbar'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
     </>
   )
 }
