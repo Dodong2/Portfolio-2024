4644d4e0be4828edff39717374daa8e7d2a65442
unknown <carlseighartaliode@gmail.com>
2024-01-31 22:46:23 +0800

    reset ui

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,6 @@
-
-
 const navbar = () => {
   return (
-    <>
-    <nav className="navbar navbar-dark bg-dark fixed-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Offcanvas dark navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end text-bg-dark"  id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
-        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            
-          </li>
-        </ul>
-        
-      </div>
-    </div>
-  </div>
-</nav>
-    </>
+    <div>navbar</div>
   )
 }
 
