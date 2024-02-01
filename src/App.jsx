b63a8c9d4599e6d87496fd1ad39648206cbadaff
unknown <carlseighartaliode@gmail.com>
2024-02-01 22:20:15 +0800

    switch and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
 import Navbar from './components/navbar'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap'
+import { useState } from 'react';
 function App() {
+  const [isChecked, setIsChecked] = useState(true);
+
+ const myFunction = () => {
+  var element = document.body;
+        element.dataset.bsTheme =
+          element.dataset.bsTheme == "dark" ? "light" : "dark";
+          setIsChecked(!isChecked);
+      }
 
   return (
     <>
-      <Navbar/>
+    <Navbar/>
+    <div className="form-check form-switch mx-4">
+      <input
+        className="form-check-input p-2"
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckChecked"
+        checked={isChecked}
+        onChange={myFunction}
+      />
+    </div>
     </>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import {IoMdMenu} from 'react-icons/io'
+import style from './navbar.module.css'
+import { useState } from 'react'
+
 const navbar = () => {
+  const [isShow, setShow] = useState(false);
+
+
   return (
-    <div>navbar</div>
+    <>
+    <div className={style.body}>
+    <nav className='border border-primary bg-dark'>
+      <div className='container-fluid m-0 p-0 d-flex justify-content-between'>
+      {/* left */}
+        <div className={style.left}>
+          <ul>
+            <li><div className='d-lg-none'><IoMdMenu/></div></li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      {/* right */}
+      <div className={style.right}><button className={style.button}>click</button></div>
+      </div>
+    </nav>
+    </div>
+    </>
   )
 }
 
